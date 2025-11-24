dd292f50af22e3dfd64fa0a4a77978fb15b48e5b
Blacksmith <cryptowitharyancourselink@gmail.com>
2025-11-25 00:24:51 +0530

    Delete setup.py

--- setup.py
+++
@@ -1,7 +0,0 @@
-from setuptools import setup
-from Cython.Build import cythonize
-
-setup(
-    name="main_bot",
-    ext_modules=cythonize("main_bot.py", compiler_directives={"language_level": "3"}),
-)
